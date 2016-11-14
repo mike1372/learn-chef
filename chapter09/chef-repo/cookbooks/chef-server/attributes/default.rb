75e17cfcdaf6a728ffc8f03f38df725a42f3560c
Michael Kempster <michaelkempster@bigpond.com>
2016-11-14 12:12:26 +1100

    Added code to install the GUI console in the recipe as well as changing variable names.

--- chapter09/chef-repo/cookbooks/chef-server/attributes/default.rb
+++ chapter09/chef-repo/cookbooks/chef-server/attributes/default.rb
@@ -1,3 +1,7 @@
 default['chef-server']['url'] = 'https://packages.chef.io/stable/el/6/chef-server-core-12.10.0-1.el6.x86_64.rpm'
-package_url = node['chef-server']['url']
-package_name = ::File.basename(package_url)
+chef_server_url = node['chef-server']['url']
+chef_server_name = ::File.basename(chef_server_url)
+
+default['chef-manage']['url'] = 'https://packages.chef.io/stable/el/6/chef-manage-2.4.4-1.el6.x86_64.rpm'
+chef_manage_url = node['chef-manage']['url']
+chef_manage_name = ::File.basename(chef_manage_url)
--- chapter09/chef-repo/cookbooks/chef-server/recipes/default.rb
+++ chapter09/chef-repo/cookbooks/chef-server/recipes/default.rb
@@ -3,16 +3,40 @@
 #
 #
 
-package_url = node['chef-server']['url']
-package_name = ::File.basename(package_url)
-package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
+chef_server_url = node['chef-server']['url']
+chef_server_name = ::File.basename(chef_server_url)
+chef_server_local_path = "#{Chef::Config[:file_cache_path]}/#{chef_server_name}"
 
-# omnibus_package is remote (i.e., a URL) let's download it
-remote_file package_local_path do
-  source package_url
+chef_manage_url = node['chef-manage']['url']
+chef_manage_name = ::File.basename(chef_manage_url)
+chef_manage_local_path = "#{Chef::Config[:file_cache_path]}/#{chef_manage_name}"
+
+# Download the Chef Server
+remote_file chef_server_local_path do
+  source chef_server_url
 end
 
-package package_local_path
+# Download the Chef Management Console
+remote_file chef_manage_local_path do
+  source chef_manage_url
+end
 
-# reconfigure the installation
-execute 'chef-server-ctl reconfigure'
+# Install the Chef Server
+package chef_server_name do
+  source chef_server_local_path
+  provider Chef::Provider::Package::Rpm
+  notifies :run, 'execute[chef server reconfigure]', :immediately
+end
+
+# Install the Chef Management Console
+package chef_manage_name do
+  source chef_manage_local_path
+  provider Chef::Provider::Package::Rpm
+  notifies :run, 'execute[chef server reconfigure]', :immediately
+end
+
+# Reconfigure the installation - needed after installing things
+execute 'chef server reconfigure' do
+  command 'chef-server-ctl reconfigure'
+  action :nothing
+end
