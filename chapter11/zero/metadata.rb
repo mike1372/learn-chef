dfaff8b75a0503edd6fb6a45b894a765494a9fb2
Michael Kempster <michaelkempster@bigpond.com>
2016-11-17 11:46:13 +1100

    Added chapter11.

---
+++ chapter11/zero/metadata.rb
@@ -0,0 +1,15 @@
+name 'zero'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures zero'
+long_description 'Installs/Configures zero'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/zero/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/zero' if respond_to?(:source_url)
