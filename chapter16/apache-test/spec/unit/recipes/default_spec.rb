ca4bd87c19247a8e33d4a0a6191268ac31705f28
Michael Kempster <michaelkempster@bigpond.com>
2016-11-21 16:54:07 +1100

    Added chefspec stuff.

--- chapter16/apache-test/spec/unit/recipes/default_spec.rb
+++ chapter16/apache-test/spec/unit/recipes/default_spec.rb
@@ -6,15 +6,23 @@
 
 require 'spec_helper'
 
-describe 'apache-test::default' do
-  context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+# describe 'apache-test::default' do
+#   context 'When all attributes are default, on an unspecified platform' do
+#     let(:chef_run) do
+#       runner = ChefSpec::ServerRunner.new
+#       runner.converge(described_recipe)
+#     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+#     it 'converges successfully' do
+#       expect { chef_run }.to_not raise_error
+#     end
+#   end
+# end
+
+describe 'apache-test::default' do
+  let (:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  it 'installs apache2' do
+    expect(chef_run).to install_package('httpd')
   end
 end
